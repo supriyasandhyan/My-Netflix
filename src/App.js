d774899f388526771ca0f40ac1cdf423e67dc124
Supriya Premraj Sandhyan <47947363+supriyasandhyan@users.noreply.github.com>
2024-04-24 13:32:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import More from "./Components/More";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Home/>
+      <More/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
